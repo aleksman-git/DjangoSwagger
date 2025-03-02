c75aa67cdd5a2f530df951d83156421e033669f3
Aleksandr <smolinav1987@yandex.ru>
2025-03-02 18:16:16 +0300

    initial commit

--- django_swagger_postgres/django_app/models.py
+++ django_swagger_postgres/django_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Login(models.Model):
-    email = models.CharField(max_length=50, null=True, blank=False, unique=True)
+    email = models.CharField(max_length=50, null=False, blank=True, unique=True)
 
     def __str__(self):
         return self.email
--- django_swagger_postgres/django_app/views.py
+++ django_swagger_postgres/django_app/views.py
@@ -10,12 +10,18 @@ class GetMethod(viewsets.ModelViewSet):
     serializer_class = LoginSerializer
     http_method_names = ['get', 'post', 'put', 'delete']
 
-    @swagger_auto_schema(operation_description="login_list")
+    @swagger_auto_schema(operation_description="login_list",responses={
+            200: 'list of logins',
+            404: 'list of logins not found'
+        })
     def list(self, request, *args, **kwargs):
         login_list = list(Login.objects.all().values())
         return Response(login_list)
 
-    @swagger_auto_schema(operation_description="login_read")
+    @swagger_auto_schema(operation_description="login_read",responses={
+            200: 'your login',
+            404: 'your login not found'
+        })
     def retrieve(self, request, *args, **kwargs):
         login_list_id = list(Login.objects.filter(id=kwargs['pk']).values())
         return Response(login_list_id)
@@ -55,7 +61,10 @@ class PasswordView(viewsets.ModelViewSet):
     serializer_class = PasswordSerializer
     http_method_names = ['get', 'post', 'put']
 
-    @swagger_auto_schema(operation_description="password_list")
+    @swagger_auto_schema(operation_description="password_list",responses={
+            200: 'list of passwords',
+            404: 'list of passwords not found'
+        })
     def list(self, request, *args, **kwargs):
         password_list = list(Password.objects.all().values())
         return Response(password_list)
@@ -69,7 +78,10 @@ class PasswordView(viewsets.ModelViewSet):
         else:
             return Response({"message": "Can't ADD"})
 
-    @swagger_auto_schema(operation_description="password_read")
+    @swagger_auto_schema(operation_description="password_read",responses={
+            200: 'your password',
+            404: 'your password not found'
+        })
     def retrieve(self, request, *args, **kwargs):
         password_list_id = list(Password.objects.filter(id=kwargs['pk']).values())
         return Response(password_list_id)
